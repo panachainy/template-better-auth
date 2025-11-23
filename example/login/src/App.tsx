a99862dc46c4a3a24bbecbb0b91d97732a19f49c
Panachai <panachai.ny@gmail.com>
2025-11-23 19:23:00 +0700

    add plugin client

--- example/login/src/App.tsx
+++ example/login/src/App.tsx
@@ -1,6 +1,10 @@
 import { createAuthClient } from 'better-auth/client'
+import { anonymousClient } from "better-auth/client/plugins"
 
 const authClient = createAuthClient({
+  plugins: [
+    anonymousClient()
+  ],
   baseURL: 'http://localhost:3001', // Replace with your auth server URL
   basePath: '/api/v1/auth',
 })
@@ -17,17 +21,28 @@ function App() {
     }
   }
 
+  const handleAnonymousLogin = async () => {
+    try {
+      const result = await authClient.signIn.anonymous()
+      console.log('Login successful:', result)
+    } catch (error) {
+      console.error('Login failed:', error)
+    }
+  }
+
+
+
   return (
     <div>
       <h1>OAuth Login Example</h1>
-      <button type="button" onClick={() => handleOAuthLogin('google')}>
-        Login with Google
-      </button>
-      <button type="button" onClick={() => handleOAuthLogin('github')}>
-        Login with GitHub
-      </button>
       <button type="button" onClick={() => handleOAuthLogin('line')}>
         Login with LINE
+      </button>
+
+      <h1>Other Login Example</h1>
+
+      <button type="button" onClick={() => handleAnonymousLogin()}>
+        Login with Anonymous
       </button>
       {/* Add more providers as needed */}
     </div>
